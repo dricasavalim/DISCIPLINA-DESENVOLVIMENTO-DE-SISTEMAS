dfbd2f116b0977d64c549986ff27f7f85daea815
dricasavalim <105366337+dricasavalim@users.noreply.github.com>
2023-05-26 16:39:26 -0300

    Add files via upload

---
+++ 001.cpp
@@ -0,0 +1,16 @@
+/*
+Fa√ßa um Programa que mostre a mensagem "Alo mundo" na tela.
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <cmath>
+#include <cstring>
+int main()
+{
+    printf("Ol·");
+
+}
+{
+    printf("mundo");
+    return 0;
+}
