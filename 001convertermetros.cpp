29bb00f12d628f258fb99d010bad8393f5e7964f
dricasavalim <105366337+dricasavalim@users.noreply.github.com>
2024-02-19 18:41:57 -0300

    Update and rename 005.cpp to 001convertermetros.cpp

--- 001convertermetros.cpp
+++ 001convertermetros.cpp
@@ -2,15 +2,17 @@
 Faça um Programa que converta metros para centímetros.
 */
 #include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+
 int main()
 {
     float metros, centimetros;
+    
     printf("Digite a medida em metros: ");
     scanf("%f", &metros);
+    
     centimetros = metros * 100;
-    printf("%.2fm correspondem a %.2fcm", metros, centimetros);
+    
+    printf("%.2fm correspondem a %.2fcm\n", metros, centimetros);
+
     return 0;
 }
