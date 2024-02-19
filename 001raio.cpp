00ea55ffba97e51674174cc9e69a30dd80350fbd
dricasavalim <105366337+dricasavalim@users.noreply.github.com>
2024-02-19 18:42:48 -0300

    Update and rename 006.cpp to 001raio.cpp

--- 001raio.cpp
+++ 001raio.cpp
@@ -2,15 +2,18 @@
 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 */
 #include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+#include <math.h>
+
 int main()
 {
     float raio, area;
+    
     printf("Digite o valor do raio: ");
     scanf("%f", &raio);
+    
     area = M_PI * pow(raio, 2);
-    printf("A area eh %f", area);
+    
+    printf("A area eh %.2f\n", area);
+
     return 0;
 }
