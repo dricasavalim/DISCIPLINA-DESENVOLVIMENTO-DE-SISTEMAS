8f4a84a53018b87ae08c27c2850586ba56a857a3
dricasavalim <105366337+dricasavalim@users.noreply.github.com>
2024-02-19 18:35:56 -0300

    Update and rename 001.cpp to 001 ola mundo.cpp

---
+++ 001 ola mundo.cpp
@@ -0,0 +1,10 @@
+/*
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Olá mundo\n");  // Adicionei "\n" para pular linha no final da mensagem
+    return 0;
+}
