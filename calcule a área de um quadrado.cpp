94d8fc3d63ee14f11f99fe42854cfc4d13bd3680
dricasavalim <105366337+dricasavalim@users.noreply.github.com>
2024-02-19 18:32:29 -0300

    Update and rename 007.cpp to calcule a área de um quadrado.cpp

--- calcule a área de um quadrado.cpp
+++ calcule a área de um quadrado.cpp
@@ -3,16 +3,14 @@ Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro
 desta área para o usuário.
 */
 #include <stdio.h>
-#include <ctype.h>
-#include <cmath>
-#include <cstring>
+
 int main()
 {
     float lado, area, dobro;
     printf("Digite o lado do quadrado: ");
     scanf("%f", &lado);
-    area = pow(lado, 2);
+    area = lado * lado;
     dobro = area * 2;
-    printf("O dobro da area do quadrado de lado %.2f eh %.2f", lado, dobro);
+    printf("O dobro da area do quadrado de lado %.2f eh %.2f\n", lado, dobro);
     return 0;
 }
